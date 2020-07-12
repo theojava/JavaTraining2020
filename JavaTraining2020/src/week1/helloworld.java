d84126bdff5b4e97825769471d21f01f61c89d99
theojava <theo.betto@gmail.com>
2020-07-12 10:56:07 -0400

    new program

---
+++ JavaTraining2020/src/week1/helloworld.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class helloworld {
+    /**
+     * main() function executes when Java program is run.
+     * 
+     * public = accessible to all
+     * static = does not need to be declared to be used
+     * void = returns nothing
+     */
+    public static void main(String[] args) {
+        // this is a comment, press [F6] to run me
+        System.out.println("Hello, World!!!!!");
+    }
+}
+
